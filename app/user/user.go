469a126f15ed08e360f182aefdda96a1366a2270
ranxuening <colbert.ran@outlook.com>
2024-01-20 14:05:52 +0800

    优化：调整项目结构，公用package调整至infrastructure层

--- app/user/user.go
+++ app/user/user.go
@@ -3,23 +3,23 @@ package user
 import (
 	"net/http"
 
-	"github.com/cloud-disk/app/common"
+	"github.com/cloud-disk/app/types/result"
 	"github.com/gin-gonic/gin"
 )
 
 func GetUserInfo(c *gin.Context) {
 	userName := c.DefaultQuery("user_name", "")
 	if userName == "" {
-		c.JSON(http.StatusBadRequest, common.Failure(http.StatusBadRequest, "the request user name is empty"))
+		c.JSON(http.StatusBadRequest, result.Failure(http.StatusBadRequest, "the request user name is empty"))
 		return
 	}
 
 	rsp, err := queryUserInfoTable()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, common.Failure(http.StatusInternalServerError, "query user info error"))
+		c.JSON(http.StatusInternalServerError, result.Failure(http.StatusInternalServerError, "query user info error"))
 		return
 	}
-	c.JSON(http.StatusOK, common.Success(rsp))
+	c.JSON(http.StatusOK, result.Success(rsp))
 }
 
 func queryUserInfoTable() (*UserInfo, error) {
--- internal/constants/constants.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const (
-	ConfigFilePath = "/conf/cloud_disk.yaml"
-	RootPath       = "/Users/ranxuening/Documents/Projects/CloudDisk"
-
-	PrefixURL  = "/cloud_disk/v1"
-	TimeFormat = "2006-01-02 15:04:05.000"
-)
