5e80236e937bc929028672f015fd8ece4c437efd
ranxuening <colbert.ran@outlook.com>
2024-03-24 12:00:20 +0800

    V0.1 优化了脚本，增加了vscode环境配置

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,10 +1,13 @@
 #!usr/bin/env bash
 # 项目根目录
 workspace=$(cd $(dirname "$0")/.. || exit; pwd)
+
 # 二进制包名称
-pkg_name=cloud_disk
+pkg_name=task-done
+
 # 指定启动脚本的用户
 user=ranxuening
+
 # 脚本日志输出目录
 log_path=$workspace/logs/system.log
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,26 @@
 #!usr/bin/env bash
 # 只要出错就退出
 set -o errexit
+
 # 只要有未定义的变量就退出
-set -o nounset
+# set -o nounset
+
 # 只要管道中任何一个命令失败就退出
 set -o pipefail
+
 # 开启shell调试模式，打印命令和输出
 set -x
-# 导入公用脚本
+
+# 导入公用脚本，并检查是否执行成功
 source service.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to load service.sh"
+fi
 
 # 检查当前执行脚本的用户
-if ["$(whoami)" != $user]; then
-    log "Current user is not ${user}"
+current_user=$(whoami)
+if [[ "${current_user}" != "$user" ]]; then
+    log "The current user ${current_user} is not ${user} as expected"
     exit 1
 fi
 
@@ -22,7 +30,7 @@ source build.sh
 log "Build completed! Begin to run!"
 # 启动前检查进程是否存在
 if ps aux | grep -v grep | grep "$pkg_name" > /dev/null; then
-    log "Process $pkg_name is running. Killing the process..."
+    log "Old process $pkg_name is running. Killing the pid..."
     
     # 获取进程ID并杀死进程
     process_id=$(ps aux | grep -v grep | grep "$pkg_name" | awk '{print $2}')
@@ -32,4 +40,4 @@ if ps aux | grep -v grep | grep "$pkg_name" > /dev/null; then
 fi
 
 # 在后台启动进程
-nohup ./${pkg_name} 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S|SYSTEM|"), $0; fflush(); }' > $log_path &
+nohup ${pkg_name} 2>&1 | awk '{ print strftime("%Y-%m-%d %H:%M:%S|SYSTEM|"), $0; fflush(); }' >> $log_path &
